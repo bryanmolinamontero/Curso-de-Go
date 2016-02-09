23b0dea28554f115829c6df2d90657a6b962a964
Bryan Molina <bryanux@hotmail.com>
2016-02-09 15:04:12 -0500

    First Commit

---
+++ 1. Variables.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	//var x,y,z int // valor por defecto cero 0
+	//var cadena string // valor por defecto cadena vacia ""
+	//var bandera bool // valor por defecto false
+	// var array [] string
+	// en GO cuando se declara una variable y no se la usa da error y no permite compilar
+
+	nombre := "Mi cadena" // tipado dinamico de go que declara una variable depende de lo que se está asignando
+	fmt.Println(nombre)
+
+}
